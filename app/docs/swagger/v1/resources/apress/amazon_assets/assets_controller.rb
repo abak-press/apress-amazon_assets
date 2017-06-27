0a1c94823a30dd85c21799e12009f3f6aee40798
Denis Korobicyn <deniskorobitcin@gmail.com>
2017-06-27 09:12:27 +0500

    fix: remove amazon api from documentation page
    https://jira.railsc.ru/browse/PC4-19812

--- app/docs/swagger/v1/resources/apress/amazon_assets/assets_controller.rb
+++ app/docs/swagger/v1/resources/apress/amazon_assets/assets_controller.rb
@@ -1,44 +1,40 @@
-Apress::Documentation.build(:'apress-amazon_assets') do
-  document(:http_api, title: 'HTTP API') do
-    document(:v1, title: 'Версия 1') do
-      swagger_bind do
-        business_desc 'Просмотр ассета'
+module Swagger
+  module V1
+    module Resources
+      module Apress
+        module AmazonAssets
+          class AssetsController < ::Apress::Documentation::Swagger::Schema
+            swagger_path('/asssets/{id}') do
+              operation :get do
+                key :produces, ['application/json']
 
-        publicity 'Защищенный'
+                key :description,
+                    "-" \
+                    "<h4>Allowed user roles:</h4> User."
+                key :operationId, 'asssetShow'
+                key :tags, ['amazon_assets']
 
-        consumers 'Нет'
-
-        tests 'В геме apress-amazon_assets'
-
-        swagger_path('/asssets/{id}') do
-          operation :get do
-            key :produces, ['application/json']
-
-            key :description,
-                "-" \
-                "<h4>Allowed user roles:</h4> User."
-            key :operationId, 'asssetShow'
-            key :tags, ['amazon_assets']
-
-            parameter do
-              key :name, :id
-              key :in, :path
-              key :description, "ИД ассета"
-              key :type, :integer
-              key :format, :int64
-            end
-
-            response 200 do
-              key :description, 'Success response'
-              schema type: 'object' do
-                property :asset do
-                  key :'$ref', :'Swagger::V1::Models::Apress::AmazonAssets::Asset'
+                parameter do
+                  key :name, :id
+                  key :in, :path
+                  key :description, "ИД ассета"
+                  key :type, :integer
+                  key :format, :int64
                 end
+
+                response 200 do
+                  key :description, 'Success response'
+                  schema type: 'object' do
+                    property :asset do
+                      key :'$ref', :'Swagger::V1::Models::Apress::AmazonAssets::Asset'
+                    end
+                  end
+                end
+
+                extend Swagger::V1::DefaultResponses::Unauthenticated
+                extend Swagger::V1::DefaultResponses::NotFound
               end
             end
-
-            extend Swagger::DefaultResponses::Unauthenticated
-            extend Swagger::DefaultResponses::NotFound
           end
         end
       end
